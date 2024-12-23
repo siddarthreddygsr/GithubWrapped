0ad625964c8f60ee0b3124a5278b7bcbdc54f549
Siddarthreddygsr <siddarthreddygsr@gmail.com>
2024-12-23 08:12:53 +0530

    analytics
    Signed-off-by: Siddarthreddygsr <siddarthreddygsr@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -29,6 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
